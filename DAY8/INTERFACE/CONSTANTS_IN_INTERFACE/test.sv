20818b1a8e5a3e7b6611f1169c4ed57bba862761
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-12-30 23:34:53 +0530

    CONSTANTS_IN_INTERFACE : test

---
+++ DAY8/INTERFACE/CONSTANTS_IN_INTERFACE/test.sv
@@ -0,0 +1,18 @@
+// Constants in Interface (parameter vs localparam)
+// test
+
+module test(intf intff);
+  
+  initial begin
+    $monitor("data type size of a = %0d | a = %0d | z = %0d, data type size of Z = %0d", $size(intff.a), intff.a, intff.z, $size(intff.z));
+    
+    // initialization 
+    intff.a = 3114;	#5;
+    
+    intff.a = 205431;	#5;
+    
+    intff.a = 66;	#5;
+    
+    intff.a = 1906132;  #5;
+  end
+endmodule
